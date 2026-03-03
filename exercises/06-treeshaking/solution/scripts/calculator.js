9d777fca8b9392cbe1362c6a786b5fef43c947c6
Hadeeb Farhan <hfkakkadavath@suki.ai>
2026-03-03 10:15:49 +0530

    exercise 06 update input

---
+++ exercises/06-treeshaking/solution/scripts/calculator.js
@@ -0,0 +1,17 @@
+// Calculator module (CommonJS format)
+import { add, subtract, multiply } from "./utils.js";
+
+function calculate(operation, a, b) {
+  switch (operation) {
+    case "add":
+      return add(a, b);
+    case "subtract":
+      return subtract(a, b);
+    // case "multiply":
+    //   return multiply(a, b);
+    default:
+      throw new Error("Unknown operation: " + operation);
+  }
+}
+
+export { calculate };
--- exercises/06-treeshaking/solution/scripts/index.js
+++ exercises/06-treeshaking/solution/scripts/index.js
@@ -1,4 +1,6 @@
-import { activate } from './search-widget.js';
-import { sendEvent } from './analytics.js';
-activate();
-sendEvent();
+import { calculate } from "./calculator.js";
+
+console.log("Calculator Demo");
+console.log("5 + 3 =", calculate("add", 5, 3));
+console.log("10 - 4 =", calculate("subtract", 10, 4));
+// console.log("6 * 7 =", calculate("multiply", 6, 7));
