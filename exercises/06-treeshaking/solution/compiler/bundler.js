c469ca1f99de4fb2f3e394545a8f134dfe6c6faf
Hadeeb Farhan <hfkakkadavath@suki.ai>
2026-03-03 11:14:29 +0530

    exercise 06 solution

--- exercises/06-treeshaking/solution/compiler/bundler.js
+++ exercises/06-treeshaking/solution/compiler/bundler.js
@@ -6,38 +6,44 @@ import * as path from 'path';
 import { traverse, parse, transformFromAstSync, types as t } from '@babel/core';
 
 /**
- * Prune the AST to only include the used identifiers and their dependencies
- * Return a new AST with the filtered body
  * @param {t.File} ast
  * @param {Set<string>} usedExports the exports the consumer of the module is actually importing
  * @returns {t.File}
  */
 function pruneAst(ast, usedExports) {
-    // Variables that are used in the module
-    // start with all the used exports
-    const usedIdentifiers = new Set(usedExports);
-
-    // Pass 1:
     // Traverse the AST to find the used identifiers
     // Check for CallExpressions: Functions that are used
     // and add them to the usedIdentifiers set
+    const usedIdentifiers = new Set(usedExports);
+
+    traverse(ast, {
+        CallExpression(callNode) {
+            if (t.isIdentifier(callNode.node.callee)) {
+                usedIdentifiers.add(callNode.node.callee.name);
+            }
+        },
+    });
 
 
-    // Hint: Use traverse from @babel/core to find the used identifiers
-
-    // Pass 2:
     // Filter ast.program.body to remove
     // 1. Function declarations not in usedIdentifiers
     // 2. Import declarations
     // 2.1 Filter out specifiers that are not in usedIdentifiers
     // 3. Export declarations
     // 3.1 Filter out specifiers that are not in usedExports
-    const body = ast.program.body.filter(x => {
-        // Hint: Use t.isFunctionDeclaration, t.isImportDeclaration, t.isExportNamedDeclaration
+    const body = ast.program.body.filter((node) => {
+        if (t.isExportNamedDeclaration(node)) {
+            node.specifiers = node.specifiers.filter((spec) => t.isIdentifier(spec.exported) && usedExports.has(spec.exported.name));
+            return node.specifiers.length > 0;
+        }
+        if (t.isImportDeclaration(node)) {
+            node.specifiers = node.specifiers.filter((spec) => t.isImportSpecifier(spec) && t.isIdentifier(spec.imported) && usedIdentifiers.has(spec.imported.name));
+            return node.specifiers.length > 0;
+        }
+        if (t.isFunctionDeclaration(node) && node.id) return usedIdentifiers.has(node.id.name);
 
-        return false;
+        return true;
     });
-
 
 
     // Create a new AST with the filtered body
@@ -48,38 +54,54 @@ function pruneAst(ast, usedExports) {
 // This function should:
 // 1. Read the file content
 // 2. Parse it into an AST
-// 3. Extract dependencies (import statements)
+// 3. Extract dependencies (import statements) with the imported names
+// 3.1 Prune the AST to only include the imported names & their dependencies
 // 4. Transform ES6 to CommonJS
 // 5. Return an object with { dependencies, code }
 /**
  * @param {string} filename
- * @returns {{dependencies: Set<string>, code: string}}
+ * @param {Set<string>} usedExports
+ * @returns {{dependencies: Map<string, string[]>, code: string}}
  */
-function createAsset(filename) {
+function createAsset(filename, usedExports) {
+
     // Hint: Use fs.readFileSync to read the file
     const content = fs.readFileSync(filename, 'utf-8');
 
-    const ast = parse(content, { sourceType: 'module' });
+    // Hint: Use parse from @babel/core to create AST with { sourceType: 'module' }
+    const ast = parse(content, {
+        sourceType: 'module'
+    });
 
     if (!ast) throw new Error('Failed to parse AST');
 
-    // Hint: Pass in the used exports to prune the AST
-    // const prunedAst = pruneAst(ast, new Set(usedExports));
+    // Prune the AST to only include the imported names
+    const prunedAst = pruneAst(ast, usedExports);
+
+
+    // Hint: Use traverse from @babel/core to find ImportDeclaration nodes
 
     // Extract dependencies
-    // Hint: Switch to using a Map with {"source path": Array<used import names>}
-    const dependencies = new Set();
-
-
-    traverse(ast, {
-        ImportDeclaration: ({ node }) => {
-            dependencies.add(node.source.value);
-            // Add the import names to the dependencies map
+    // Hint: Switch to using a Map for dependencies
+    const dependencies = new Map();
+    traverse(prunedAst, {
+        ImportDeclaration({ node }) {
+            const names = [];
+            for (const spec of node.specifiers) {
+                if (t.isImportSpecifier(spec)) {
+                    if (t.isIdentifier(spec.imported)) {
+                        names.push(spec.imported.name);
+                    }
+                }
+            }
+            dependencies.set(node.source.value, names);
         }
     });
 
-    // Hint: Use the pruned AST
-    const result = transformFromAstSync(ast, undefined, {
+
+
+    // Use @babel/plugin-transform-modules-commonjs plugin
+    const result = transformFromAstSync(prunedAst, undefined, {
         plugins: [['@babel/plugin-transform-modules-commonjs', { strict: true }]],
 
     });
@@ -102,26 +124,29 @@ function createAsset(filename) {
 /**
  * @param {string} rootDirectory
  * @param {string} entry
- * @returns {Array<{ id: string, code: string }>}
+ * @returns {Array<{ id: string, dependencies: Map<string, string[]>, code: string }>}
  */
 function createModuleGraph(rootDirectory, entry) {
-    // Hint: Store both the entry and the used exports in the queue
-    const queue = [entry];
+    // Hint: Use a queue (array) for BFS
+    // Hint: Use path.resolve and path.dirname to resolve relative paths
+    // Hint: Assign a the relative path as the id
+    /**@type {Array<{ entry: string, usedExports: string[] }>} */
+    const queue = [{ entry, usedExports: [] }];
 
     const graph = [];
 
     while (queue.length > 0) {
         // Loop through the queue and create an asset for each entry
         // Add the asset to the graph
-        const entry = /**@type {string} */ (queue.shift());
-        const asset = createAsset(path.resolve(rootDirectory, entry));
-        graph.push({ id: `./${entry}`, code: asset.code });
+        const { entry, usedExports } = /**@type {{ entry: string, usedExports: string[] }} */ (queue.shift());
+        const asset = createAsset(path.resolve(rootDirectory, entry), new Set(usedExports));
+        graph.push({ id: `./${entry}`, ...asset });
         // For each dependency,
         //  > resolve the relative path to the root directory and add it to the queue
-        for (const relativePath of asset.dependencies) {
+        for (const [relativePath, imported] of asset.dependencies) {
             const relativePathToRoot = path.join(path.dirname(entry), relativePath);
-            queue.push(relativePathToRoot);
-        };
+            queue.push({ entry: relativePathToRoot, usedExports: imported });
+        }
     }
 
     return graph;
